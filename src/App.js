36587e0c2343189b11642bf60c51de7ee0dfcdcb
Saloni Verma <saloni.verma@centricconsulting.com>
2024-02-08 15:48:57 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// App.js
-
 import React from 'react';
 import './App.css';
 import Header from './components/Header.jsx';
--- src/components/Addition.jsx
+++ src/components/Addition.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import './Addition.css';
+import { Calendar } from 'primereact/calendar';
+import { useNavigate } from 'react-router-dom';
+
 
 
 export default function Addition() {
@@ -7,22 +10,28 @@ export default function Addition() {
     const [formData, setFormData] = useState({
         chasisNo: '',
         engineNo: '',
-        hpaFrom: '',
-        hpaUpto: '',
+        hpaFrom: null,
+        hpaUpto: null,
         docUrl: '',
         regNo: ''
     });
 
     const [formErrors, setFormErrors] = useState({});
-    const [successMessage, setSuccessMessage] = useState('');
+    const [modalMessage, setModalMessage] = useState('');
+    const [showModal, setShowModal] = useState(false);
+
+    const navigate = useNavigate();
+
+
 
     // Fxn to handle form input change
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
+            setFormData({
+                ...formData,
+                [name]: value
+            });
+        
     };
 
     // Fxn to validate form data
@@ -31,26 +40,15 @@ export default function Addition() {
 
         if (!formData.chasisNo.trim()) {
             errors.chasisNo = 'Chasis No is required';
+        } else if (!/^\d{4,17}$/.test(formData.chasisNo.trim())) {
+            errors.chasisNo = 'Chasis No must be between 4 to 17 digits and contain no spaces';
         }
 
 
         if (!formData.regNo.trim()) {
             errors.regNo = 'Registration No is required';
-        } else if (formData.regNo.trim().length < 10) {
-            errors.regNo = 'Registration No must be at least 10 characters';
-        }
-
-        const datePattern = /^\d{4}-\d{2}-\d{2}$/;
-        if (!formData.hpaFrom.trim()) {
-            errors.hpaFrom = 'Hpa from is required';
-        } else if (!datePattern.test(formData.hpaFrom.trim())) {
-            errors.hpaFrom = 'Hpa from be in YYYY-MM-DD format';
-        }
-
-        if (!formData.hpaUpto.trim()) {
-            errors.hpaUpto = 'Hpa Upto is required';
-        } else if (!datePattern.test(formData.hpaUpto.trim())) {
-            errors.hpaUpto = 'Hpa Upto be in YYYY-MM-DD format';
+        } else if (!/^\d{10,}$/.test(formData.regNo.trim())) {
+            errors.regNo = 'Registration No must be at least 10 digits and contain no spaces';
         }
 
         setFormErrors(errors);
@@ -79,7 +77,7 @@ export default function Addition() {
 
         if (validateForm()) {
             // You can perform further actions if the form is valid, such as sending the form data to a server
-            console.log('Form submitted:', formData);
+            console.log('Addition Form submitted:', formData);
             // Reset form fields after submission
             setFormData({
                 chasisNo: '',
@@ -89,16 +87,30 @@ export default function Addition() {
                 docUrl: '',
                 regNo: ''
             });
-            // Set success message
-            setSuccessMessage('Form submitted successfully!');
-            // Clear success message after 5 seconds
+            setModalMessage('Addition Form submitted successfully!');
+            setShowModal(true);
             setTimeout(() => {
-                setSuccessMessage('');
-            }, 5000);
+                setShowModal(false);
+                navigate('/'); // Redirect to the home page
+            }, 4000);
         } else {
-            console.log('Form validation failed');
+            setModalMessage('Something went wrong. Please check again.');
+            setShowModal(true);
+            setTimeout(() => {
+                setShowModal(false);
+            }, 4000);
         }
     };
+
+    function CustomModal({ message }) {
+        return (
+            <div className="modal">
+                <div className="modal-content">
+                    <p>{message}</p>
+                </div>
+            </div>
+        );
+    }
 
 
     return (
@@ -107,7 +119,7 @@ export default function Addition() {
             <form onSubmit={handleSubmit} className="addition-form">
                 <div className="form-group">
                     <label htmlFor="chasisNo">Chasis No :</label>
-                    <input type="text" id="chasisNo" placeholder="Enter Chasis Number" name="chasisNo" value={formData.chasisNo} onChange={handleChange} />
+                    <input className={formErrors.chasisNo ? "error-input" : ""} type="text" id="chasisNo" placeholder="Enter Chasis Number" name="chasisNo" value={formData.chasisNo} onChange={handleChange} />
                     {formErrors.chasisNo && <span className="error">{formErrors.chasisNo}</span>}
                 </div>
                 <div className="form-group">
@@ -116,13 +128,27 @@ export default function Addition() {
                 </div>
                 <div className="form-group">
                     <label htmlFor="hpaFrom">HPA From :</label>
-                    <input type="text" id="hpaFrom" name="hpaFrom" placeholder="Date(YYYY-MM-DD)" value={formData.hpaFrom} onChange={handleChange} />
-                    {formErrors.hpaFrom && <span className="error">{formErrors.hpaFrom}</span>}
+                    <Calendar
+                        id="hpaFrom"
+                        name="hpaFrom"
+                        placeholder="Date(YYYY-MM-DD)"
+                        value={formData.hpaFrom}
+                        onChange={handleChange}
+                        showIcon
+                        dateFormat="yy-mm-dd"
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="hpaUpto">HPA Upto:</label>
-                    <input type="text" id="hpaUpto" name="hpaUpto" placeholder="Date(YYYY-MM-DD)" value={formData.hpaUpto} onChange={handleChange} />
-                    {formErrors.hpaUpto && <span className="error">{formErrors.hpaUpto}</span>}
+                    <Calendar
+                        id="hpaUpto"
+                        name="hpaUpto"
+                        placeholder="Date(YYYY-MM-DD)"
+                        value={formData.hpaUpto}
+                        onChange={handleChange}
+                        showIcon
+                        dateFormat="yy-mm-dd"
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="docUrl">Document Url :</label>
@@ -130,7 +156,7 @@ export default function Addition() {
                 </div>
                 <div className="form-group">
                     <label htmlFor="regNo">Registration No :</label>
-                    <input type="text" id="regNo" name="regNo" placeholder="Enter Registration Number" value={formData.regNo} onChange={handleChange} />
+                    <input className={formErrors.regNo ? "error-input" : ""} type="text" id="regNo" name="regNo" placeholder="Enter Registration Number" value={formData.regNo} onChange={handleChange} />
                     {formErrors.regNo && <span className="error">{formErrors.regNo}</span>}
                 </div>
                 <div className="button-group">
@@ -138,7 +164,7 @@ export default function Addition() {
                     <button type="button" onClick={handleReset} className="reset-btn">Reset</button>
                 </div>
             </form>
-            {successMessage && <div className="success">{successMessage}</div>}
+            {showModal && <CustomModal message={modalMessage} />}
         </div>
     )
 }
